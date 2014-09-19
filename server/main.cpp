a444640920fdd610f1ea76411c55ce057db7bc6c
newenclave <pikratacid@gmail.com>
2014-09-19 18:31:23 +0400

    subsystems

--- server/main.cpp
+++ server/main.cpp
@@ -6,12 +6,16 @@
 #include "google/protobuf/descriptor.h"
 
 namespace vserver = vtrc::server;
+namespace vcommon = vtrc::common;
+using namespace fr;
 
 int main( int argc, const char **argv )
 {
+    vcommon::pool_pair pp( 0 );
+    server::application app( pp );
 
-
-
+    pp.stop_all( );
+    pp.join_all( );
     google::protobuf::ShutdownProtobufLibrary( );
     return 0;
 }
