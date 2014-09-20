f563ab96bf0b4e0107d3911355db7054054dd56b
V.K <kettunen.villu@gmail.com>
2014-09-20 21:26:51 +0400

    proto

--- console-client/main.cpp
+++ console-client/main.cpp
@@ -3,6 +3,7 @@
 
 #include "client-core/fr-client.h"
 #include "command-iface.h"
+#include "cmd-list.h"
 
 #include "boost/program_options.hpp"
 #include "boost/system/error_code.hpp"
@@ -15,6 +16,17 @@ namespace {
 
     typedef std::map<std::string, fr::cc::command_sptr> command_map;
     typedef vtrc::shared_ptr<vcommon::pool_pair> pool_pair_sptr;
+
+    command_map::value_type cmd( fr::cc::command_sptr ptr )
+    {
+        return std::make_pair( ptr->name( ), ptr );
+    }
+
+    void init_map( command_map &m )
+    {
+        m.insert( cmd( fr::cc::cmd::fs::create( ) ) );
+        m.insert( cmd( fr::cc::cmd::gpio::create( ) ) );
+    }
 
     void fill_common_options( po::options_description &desc )
     {
@@ -75,6 +87,21 @@ namespace {
         return vm;
     }
 
+    po::variables_map parse_command( int argc, const char **argv,
+                                     fr::cc::command_sptr command,
+                                     po::options_description &desc )
+    {
+        command->add_options( desc );
+        po::variables_map vm;
+        po::parsed_options parsed (
+            po::command_line_parser( argc, argv )
+                .options(desc)
+                .run( ));
+        po::store(parsed, vm);
+        po::notify(vm);
+        return vm;
+    }
+
     void show_init_help( const po::options_description &desc,
                          const command_map &cm)
     {
@@ -89,20 +116,22 @@ namespace {
         for( command_map::const_iterator b(cm.begin( )),
              e(cm.end( )); b!=e; ++b )
         {
-            std::cout << "\t" << std::setw(20) << b->first
+            std::cout << "\t" << b->first
                       << ": " << b->second->desc( )
                       << "\n";
         }
     }
 
-    void show_command_help( fr::cc::command_sptr &command )
+    void show_command_help( const po::options_description &cdesc,
+                            fr::cc::command_sptr &command )
     {
         po::options_description desc;
         command->add_options( desc );
 
+        std::cout << cdesc << "\n";
         std::cout << "Usage: ferro_remote_client -c " << command->name( )
                   << " [opts]\n"
-                  << "Options:\n" << desc << "\n";
+                  << "Command options: \n" << desc << "\n";
     }
 
 
@@ -114,11 +143,13 @@ namespace {
 
 int main( int argc, const char **argv ) try
 {    
-    po::options_description desc("Allowed options");
+    po::options_description desc("Common options");
     fill_common_options( desc );
     po::variables_map vm( parse_common( argc, argv, desc ) );
 
     command_map cm;
+
+    init_map( cm );
 
     fr::cc::command_sptr current_command;
 
@@ -135,16 +166,16 @@ int main( int argc, const char **argv ) try
     }
 
     if( vm.count("help") ) {
-        current_command.get( ) ? show_command_help( current_command )
+        current_command.get( ) ? show_command_help( desc, current_command )
                                : show_init_help( desc, cm );
         return 0;
     }
 
-//    if( !current_command.get( ) ) {
-//        std::cout << "Invalid command '<empty>'\n";
-//        show_init_help( desc, cm );
-//        return 2;
-//    }
+    if( !current_command.get( ) ) {
+        std::cout << "Invalid command '<empty>'\n";
+        show_init_help( desc, cm );
+        return 2;
+    }
 
     std::string server( vm.count("server")
                         ? vm["server"].as<std::string>( )
@@ -156,15 +187,19 @@ int main( int argc, const char **argv ) try
         return 3;
     }
 
+
     pool_pair_sptr pp(pp_from_cmd( vm ));
 
     fr::client::core::client client(*pp);
 
     client.connect( server );
-    //client.async_connect( server, connect_success );
-    //client.disconnect(  );
 
-    //current_command->exec( vm, client );
+    vm = parse_command( argc, argv, current_command, desc );
+
+    //client.async_connect( server, connect_success );
+    client.disconnect(  );
+
+    current_command->exec( vm, client );
 
     pp->get_io_pool( ).attach( );
 
