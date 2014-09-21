56d4058f3bf898cc981116deb338d358627f4cc9
V.K <kettunen.villu@gmail.com>
2014-09-21 18:21:50 +0400

    server

--- server/poll-reactor.h
+++ server/poll-reactor.h
@@ -1,6 +1,18 @@
 #ifndef FR_POLLREACTOR_H
 #define FR_POLLREACTOR_H
 
+namespace fr { namespace server {
 
+    class poll_reactor {
+        struct   impl;
+        impl    *impl_;
+
+    public:
+
+        poll_reactor( );
+        ~poll_reactor( );
+    };
+
+}}
 
 #endif // FR_POLLREACTOR_H
