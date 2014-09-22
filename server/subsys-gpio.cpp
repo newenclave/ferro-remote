6e82f18c53a599ca138d002c0035a24304cb67d1
V.K <kettunen.villu@gmail.com>
2014-09-22 19:10:23 +0400

    server

--- server/subsys-gpio.cpp
+++ server/subsys-gpio.cpp
@@ -84,11 +84,17 @@ namespace fr { namespace server { namespace subsys {
     void gpio::start( )
     {
         impl_->reg_creator( gpio_impl::name( ),  create_service );
+        std::cout << __LINE__ << "\n";
         gpio_inst i(74);
+        std::cout << __LINE__ << "\n";
         i.exp(  );
+        std::cout << __LINE__ << "\n";
         i.set_direction( server::gpio::DIRECT_OUT );
+        std::cout << __LINE__ << "\n";
         i.set_value( 1 );
+        std::cout << __LINE__ << "\n";
         i.unexp( );
+        std::cout << __LINE__ << "\n";
     }
 
     void gpio::stop( )
