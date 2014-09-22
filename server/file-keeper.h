31395b427fcb7ddd8890d18543f753fdcffc5a7e
V.K <kettunen.villu@gmail.com>
2014-09-22 23:00:00 +0400

    gpio

---
+++ server/file-keeper.h
@@ -0,0 +1,41 @@
+#ifndef FR_FILEKEEPER_H
+#define FR_FILEKEEPER_H
+
+#include <unistd.h>
+#include "vtrc-memory.h"
+
+namespace fr { namespace server {
+
+    struct file_keeper {
+
+        int id_;
+
+        file_keeper( int fd )
+            :id_(fd)
+        { }
+
+        ~file_keeper( )
+        {
+            if( -1 != id_ ) {
+                close( id_ );
+            }
+        }
+
+        int release( )
+        {
+            int tmp = id_;
+            id_ = -1;
+            return tmp;
+        }
+
+        int hdl( )
+        {
+            return id_;
+        }
+    };
+
+    typedef vtrc::shared_ptr<file_keeper> file_keeper_sptr;
+
+}}
+
+#endif // FILEKEEPER_H
