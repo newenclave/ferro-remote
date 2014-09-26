c69fa47452b5253e0089d2959e1fa54cc6694599
newenclave <pikratacid@gmail.com>
2014-09-26 14:46:20 +0400

    async operation event

---
+++ include/client-core/interfaces/IAsyncOperation.h
@@ -0,0 +1,19 @@
+#ifndef IASYNCOPERATION_H
+#define IASYNCOPERATION_H
+
+#include <string>
+#include "vtrc-function.h"
+#include "vtrc-stdint.h"
+
+namespace fr { namespace client {
+
+    ///
+    /// void call( error_code, data, some_numeric_value )
+    ///
+    typedef vtrc::function<
+        void ( unsigned, const std::string &, vtrc::uint64_t )
+    > async_op_callback_type;
+
+}}
+
+#endif // IASYNCOPERATION_H
---
+++ include/client-core/interfaces/IInternal.h
@@ -0,0 +1,6 @@
+#ifndef FR_IINTERNAL_H
+#define FR_IINTERNAL_H
+
+
+
+#endif // IINTERNAL_H
