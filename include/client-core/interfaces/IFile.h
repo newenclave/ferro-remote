e344fd22c2ca00697a25bd52dd1be1d321efaff7
newenclave <pikratacid@gmail.com>
2014-09-27 12:00:10 +0400

    client

--- include/client-core/interfaces/IFile.h
+++ include/client-core/interfaces/IFile.h
@@ -2,10 +2,9 @@
 #define FR_INTERFACE_FILE_H
 
 #include <string>
-#include <stddef.h>
+#include <stdint.h>
 
 #include "vtrc-function.h"
-#include "vtrc-stdint.h"
 
 namespace fr { namespace client {
 
@@ -59,10 +58,9 @@ namespace interfaces { namespace file {
     struct iface {
         virtual ~iface( ) { }
 
-        virtual vtrc::int64_t seek( vtrc::int64_t pos,
-                                    seek_whence whence ) const = 0;
+        virtual int64_t seek( int64_t pos, seek_whence whence ) const = 0;
 
-        virtual vtrc::int64_t tell( )  const = 0;
+        virtual int64_t tell( )  const = 0;
         virtual void          flush( ) const = 0;
 
         virtual size_t  read( void *data,       size_t length ) const = 0;
