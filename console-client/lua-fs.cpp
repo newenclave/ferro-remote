0023d8da40d43e719dd6f0abe0b3357b7c5240ee
newenclave <pikratacid@gmail.com>
2014-10-09 15:38:41 +0400

    lua

---
+++ console-client/lua-fs.cpp
@@ -0,0 +1,40 @@
+#include "lua-interface.h"
+
+#if FR_WITH_LUA
+
+#include <iostream>
+#include "interfaces/IOS.h"
+
+namespace fr { namespace lua {
+
+    namespace {
+        struct data: public base_data {
+
+            typedef client::interfaces::os::iface iface;
+            typedef std::unique_ptr<iface> iface_uptr;
+
+            data( client::core::client_core &cc, lua_State *L )
+            {
+
+            }
+
+            lua::objects::table_sptr get_table( )
+            {
+                objects::table_sptr ost(objects::new_table( ));
+                return ost;
+            }
+
+        };
+    }
+
+    namespace fs {
+        data_sptr init( lua_State *ls, client::core::client_core &cc )
+        {
+            lua::state lv( ls );
+            return data_sptr( new data( cc, ls ) );
+        }
+    }
+
+}}
+
+#endif
