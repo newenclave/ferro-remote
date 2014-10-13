161d5379cf237f380b91c5a2c9029ef8bff9f58d
newenclave <pikratacid@gmail.com>
2014-10-13 17:02:57 +0400

    lua

--- lua-examples/dev-random-event.lua
+++ lua-examples/dev-random-event.lua
@@ -7,17 +7,19 @@
  =  PROFIT!
 --]]
 
-function handler( err, data, params )
+function handler( err, data, params ) -- other lua thread !
     println( 'read from '..params.path, ' <- ', data ) --show this garbage
 end
 
 file = fr.client.fs.file
 
-function main( argv  )
+function main( argv ) -- main lua thread
+
     f = file.open( "/dev/random", file.RDONLY )
     file.register_for_events( f, "handler", { path = "/dev/random" } )
     while true do
         sleep( 1 )
         print( '.' ) --- some work! =)
     end
+
 end
