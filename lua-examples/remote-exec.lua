0ac51df547978515b422c9d5d5730502b18e74c4
V.K <kettunen.villu@gmail.com>
2014-10-13 23:51:53 +0400

    lua

--- lua-examples/remote-exec.lua
+++ lua-examples/remote-exec.lua
@@ -1,7 +1,7 @@
 -- execute command on the target system
 
 --[[
-    %ferro_remote_client -s xx.xx.xx.xx:xxxx -clua -e show-main-argv.lua \
+    %ferro_remote_client -s xx.xx.xx.xx:xxxx -clua -e script.lua \
         -p"command=some_remote_command"
 --]]
 
