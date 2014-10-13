caa3bb85d181fc7ee6a2b786f39dc89e81d691dc
newenclave <pikratacid@gmail.com>
2014-10-13 17:00:59 +0400

    lua

---
+++ lua-examples/dev-random-event.lua
@@ -0,0 +1,23 @@
+--[[
+ = open remote /dev/random
+ = subscribe to events
+ = do some work
+ = receive events asynchronously
+ = ....
+ = PROFIT!
+--]]
+
+function handler( err, data, params )
+    println( 'read from '..params.path, ' <- ', data ) --show this garbage
+end
+
+file = fr.client.fs.file
+
+function main( argv  )
+    f = file.open( "/dev/random", file.RDONLY )
+    file.register_for_events( f, "handler", { path = "/dev/random" } )
+    while true do
+        sleep( 1 )
+        print( '.' ) --- some work!
+    end
+end
