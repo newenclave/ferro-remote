389fae1e239045312d6a3bc6bea91f465a5b2b46
V.K <kettunen.villu@gmail.com>
2014-10-14 00:51:43 +0400

    lua

--- lua-examples/file-push.lua
+++ lua-examples/file-push.lua
@@ -1,6 +1,6 @@
 --[[
     %ferro_remote_client -s xx.xx.xx.xx:xxxx -clua -e script.lua \
-        -p"src=/remote/file/path.txt" -p"out=/path/to/local/file.txt"
+        -p"src=/remote/file/path.txt" -p"in=/path/to/local/file.txt"
 --]]
 
 open("io") -- open 'io' library for read and write file
@@ -9,18 +9,18 @@ file = fr.client.fs.file
 
 function main( argv )
 
-    print( 'Push file "', argv.src, '"', ' to "', argv.out, '"...' )
+    print( 'Push file "', argv.src, '"', ' to "', argv.in, '"...' )
 
     f = file.open( argv.out, file.flags( file.WRONLY,
                                          file.CREAT,
                                          file.TRUNC  ) )  -- remote file
-    out = io.open( argv.src, 'rb' )                       -- local file
+    infile = io.open( argv.src, 'rb' )                    -- local file
 
     d = out:read( 0 )
 
     while d do
         file.write( f, d )
-        d = out:read( 44000 )
+        d = infile:read( 44000 )
     end
 
     println( 'success!' )
