e47b4d153f578eececb4e0645fe10346e485ef96
newenclave <pikratacid@gmail.com>
2014-10-15 16:51:24 +0400

    lua

--- lua-examples/gpio-check.lua
+++ lua-examples/gpio-check.lua
@@ -1,7 +1,7 @@
 -- check for GPIO available on the remote system
 
 function main( argv )
-    println( 'GPIO is '..({ [false] = "not ",
-                            [true]  = ""
-                         }) [fr.client.gpio.available].."available" )
+    println( 'GPIO is '..({ [false] = 'not ',
+                            [true]  = ''
+                         }) [fr.client.gpio.available]..'available' )
 end
