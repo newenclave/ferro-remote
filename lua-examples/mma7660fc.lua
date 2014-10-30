c4254a28309f8fe131f045088cb2197e90fc5087
newenclave <pikratacid@gmail.com>
2014-10-30 17:06:58 +0300

    ++i2c

--- lua-examples/mma7660fc.lua
+++ lua-examples/mma7660fc.lua
@@ -6,22 +6,22 @@ open( "string" )
 
 function main ( argv )
 
-    f = file.open( "/dev/i2c-1", file.RDWR )
+    local f = file.open( "/dev/i2c-1", file.RDWR )
 
     file.ioctl( f, 0x0703, 0x4c )
     file.write( f, "\7\1" )
 
     while true do
 
-        d = file.read( f, 7 )
+        local d = file.read( f, 7 )
 
         printi( "X=",     d:byte( 1 ), "\t",
                 "Y=",     d:byte( 2 ), "\t",
                 "Z=",     d:byte( 3 ), "\t",
                 "TILT=",  d:byte( 4 ), "\t",
-                "SRST=",  d:byte( 5 ), "\t",
-                "SPCNT=", d:byte( 6 ), "\t",
-                "INTSU=", d:byte( 7 ), "\t",
+                --"SRST=",  d:byte( 5 ), "\t",
+                --"SPCNT=", d:byte( 6 ), "\t",
+                --"INTSU=", d:byte( 7 ), "\t",
                 "                       \n" )
         --sleep( 1 )
 
