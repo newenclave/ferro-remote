3d751089c27750d33fb6e730f46a90825191cbbb
V.K <kettunen.villu@gmail.com>
2014-11-01 02:20:38 +0300

    ++i2c

--- lua-examples/mma7660fc.lua
+++ lua-examples/mma7660fc.lua
@@ -6,6 +6,21 @@ i2c  = fr.client.i2c
 
 open( "string" )
 
+mma7660fc = {
+    mode      = 0x7,
+    active    = 1,
+    stand_by  = 0,
+
+    X         = 0,
+    Y         = 1,
+    Z         = 2,
+    TILT      = 3,
+}
+
+function mma7660fc_activate( dev )
+    i2c.write_byte( dev, mma7660fc.mode, mma7660fc.active )
+end
+
 function main ( argv )
 
     local err, i = i2c.open( 1 )
@@ -16,15 +31,16 @@ function main ( argv )
     end
 
     i2c.set_address( i, 0x4c )
+    mma7660fc_activate( i )
 
     while true do
 
         local d = i2c.read( i, 7 )
 
-        printi( "X=",     d:byte( 1 ), "\t",
-                "Y=",     d:byte( 2 ), "\t",
-                "Z=",     d:byte( 3 ), "\t",
-                "TILT=",  d:byte( 4 ), "\t",
+        printi( "X=",     d:byte( mma7660fc.X    + 1 ), "\t",
+                "Y=",     d:byte( mma7660fc.Y    + 1 ), "\t",
+                "Z=",     d:byte( mma7660fc.Z    + 1 ), "\t",
+                "TILT=",  d:byte( mma7660fc.TILT + 1 ), "\t",
                 --"SRST=",  d:byte( 5 ), "\t",
                 --"SPCNT=", d:byte( 6 ), "\t",
                 --"INTSU=", d:byte( 7 ), "\t",
