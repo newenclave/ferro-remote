ffcc5440a66e37af53848258129474b9f2c95fda
newenclave <pikratacid@gmail.com>
2014-10-31 14:15:09 +0300

    i2c

--- include/client-core/interfaces/II2C.h
+++ include/client-core/interfaces/II2C.h
@@ -21,10 +21,31 @@ namespace interfaces { namespace i2c {
         ,CODE_I2C_PEC         = 0x0708
     };
 
+    enum function_codes {
+         FUNC_I2C                    = 0x00000001
+        ,FUNC_10BIT_ADDR             = 0x00000002
+        ,FUNC_PROTOCOL_MANGLING      = 0x00000004 /* I2C_M_IGNORE_NAK etc. */
+        ,FUNC_SMBUS_PEC              = 0x00000008
+        ,FUNC_NOSTART                = 0x00000010 /* I2C_M_NOSTART */
+        ,FUNC_SMBUS_BLOCK_PROC_CALL  = 0x00008000 /* SMBus 2.0 */
+        ,FUNC_SMBUS_QUICK            = 0x00010000
+        ,FUNC_SMBUS_READ_BYTE        = 0x00020000
+        ,FUNC_SMBUS_WRITE_BYTE       = 0x00040000
+        ,FUNC_SMBUS_READ_BYTE_DATA   = 0x00080000
+        ,FUNC_SMBUS_WRITE_BYTE_DATA  = 0x00100000
+        ,FUNC_SMBUS_READ_WORD_DATA   = 0x00200000
+        ,FUNC_SMBUS_WRITE_WORD_DATA  = 0x00400000
+        ,FUNC_SMBUS_PROC_CALL        = 0x00800000
+        ,FUNC_SMBUS_READ_BLOCK_DATA  = 0x01000000
+        ,FUNC_SMBUS_WRITE_BLOCK_DATA = 0x02000000
+        ,FUNC_SMBUS_READ_I2C_BLOCK   = 0x04000000 /* I2C-like block xfer  */
+        ,FUNC_SMBUS_WRITE_I2C_BLOCK  = 0x08000000 /* w/ 1-byte reg. addr. */
+    };
+
     struct iface {
         virtual ~iface( ) { }
 
-        virtual vtrc::uint64_t function_mask( ) const = 0;
+        virtual vtrc::uint64_t function_mask( ) const = 0; // see function_codes
 
         virtual void   ioctl( unsigned code,    vtrc::uint64_t ) const = 0;
         virtual size_t  read( void *data,       size_t length  ) const = 0;
