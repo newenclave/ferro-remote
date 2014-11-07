7cb963d496b62c729b00852cb79b3608da3e50c2
newenclave <pikrat.acid@gmail.com>
2014-11-07 11:35:09 +0300

    Update i2c-dump.lua

--- lua-examples/i2c-dump.lua
+++ lua-examples/i2c-dump.lua
@@ -27,9 +27,9 @@ function main( argv )
             "0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f",
             "    0123456789abcdef")
 
-    --- just values 1-255
     request = {}
-
+    
+    --- values 0-255
     for i=0, 255 do
         table.insert( request, i )
     end
