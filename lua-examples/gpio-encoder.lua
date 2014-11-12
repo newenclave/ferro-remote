461e860e87ffa1b2baaf78b840e5c4969c4fdf12
V.K <kettunen.villu@gmail.com>
2014-11-13 02:15:19 +0300

    lua

--- lua-examples/gpio-encoder.lua
+++ lua-examples/gpio-encoder.lua
@@ -12,47 +12,17 @@ if not gpio.available then
     die( "GPIO is not available on the "..fr.client.server.." machine :(" )
 end
 
-current_state = 0
-current_count = 0
-last_direct   = nil
-last_start    = 0
+states      = { [true] = 0, [false] =  0 }
+directions  = { [true] = 1, [false] = -1 }
+current     = 0
 
-function change_handler( new_value, time_diff, direct )
+function change_handler( new_value, direct )
 
-    --printiln( "Value for gpio ", direct, ' ', time_diff )
+    states[direct] = new_value
 
---    stop = hight_clock( )
---    if stop - last_start > 10000000 then
---        last_direct = nil
---        current_state = 0
---        last_start = hight_clock( )
---    end
-
---    if time_diff > 10000 then -- microseconds
---        last_direct = nil
---        current_state = 0
---    end
-
-    if last_direct == direct then
-        --println( direct, ' ', current_state )
-        return
-    end
-
-    current_state = current_state + 1
-    last_direct = direct
-
-    --println( current_state, " ", direct, " " )
-    --print( current_state, " " )
-
-    if current_state == 1 then
-        if direct then
-            current_count = current_count + 1
-        else
-            current_count = current_count - 1
-        end
-        println( current_count )
-    else
-        current_state = 0
+    if new_value == 1 and states[not direct] == 1 then
+        current = current + directions[direct]
+        println( current )
     end
 
 end
@@ -66,11 +36,11 @@ function main( argv ) --- main lua thread
         die("GPIO doesn't support edge. No edge -> no events")
     end
 
-    gpio.set_edge( dev1, gpio.EDGE_FALLING )
-    gpio.set_edge( dev2, gpio.EDGE_FALLING )
+    gpio.set_edge( dev1, gpio.EDGE_BOTH )
+    gpio.set_edge( dev2, gpio.EDGE_BOTH )
 
-    gpio.register_for_change_int( dev1, change_handler, false )
-    gpio.register_for_change_int( dev2, change_handler, true )
+    gpio.register_for_change( dev1, change_handler, false )
+    gpio.register_for_change( dev2, change_handler, true )
 
     while true do  --- do work
         --print('.')
@@ -78,3 +48,41 @@ function main( argv ) --- main lua thread
     end
 
 end
+
+
+
+--current_state = 0
+--current_count = 0
+--last_direct = nil
+--last_start = 0
+--function change_handler( new_value, time_diff, direct )
+----printiln( "Value for gpio ", direct, ' ', time_diff )
+---- stop = hight_clock( )
+---- if stop - last_start > 10000000 then
+---- last_direct = nil
+---- current_state = 0
+---- last_start = hight_clock( )
+---- end
+---- if time_diff > 10000 then -- microseconds
+---- last_direct = nil
+---- current_state = 0
+---- end
+--if last_direct == direct then
+----println( direct, ' ', current_state )
+--return
+--end
+--current_state = current_state + 1
+--last_direct = direct
+----println( current_state, " ", direct, " " )
+----print( current_state, " " )
+--if current_state == 1 then
+--if direct then
+--current_count = current_count + 1
+--else
+--current_count = current_count - 1
+--end
+--println( current_count )
+--else
+--current_state = 0
+--end
+--end
