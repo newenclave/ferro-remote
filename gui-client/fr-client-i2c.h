7cc26295ea052bab064b381633cb9c40dcd2d962
newenclave <pikratacid@gmail.com>
2014-12-17 14:09:51 +0300

    qml; i2c interface

--- gui-client/fr-client-i2c.h
+++ gui-client/fr-client-i2c.h
@@ -36,29 +36,39 @@ namespace fr { namespace declarative {
         enum BusIdInvalidValue { InvalidBusId = 0xFFFFFFFF };
         enum SlaveAddrInvalidValue { InvalidSlaveAddress = 0xFFFF };
         enum I2CFunctionCodes {
-            FUNC_I2C                    = 0x00000001
-           ,FUNC_10BIT_ADDR             = 0x00000002
-           ,FUNC_PROTOCOL_MANGLING      = 0x00000004 /* I2C_M_IGNORE_NAK etc. */
-           ,FUNC_SMBUS_PEC              = 0x00000008
-           ,FUNC_NOSTART                = 0x00000010 /* I2C_M_NOSTART */
-           ,FUNC_SMBUS_BLOCK_PROC_CALL  = 0x00008000 /* SMBus 2.0 */
-           ,FUNC_SMBUS_QUICK            = 0x00010000
-           ,FUNC_SMBUS_READ_BYTE        = 0x00020000
-           ,FUNC_SMBUS_WRITE_BYTE       = 0x00040000
-           ,FUNC_SMBUS_READ_BYTE_DATA   = 0x00080000
-           ,FUNC_SMBUS_WRITE_BYTE_DATA  = 0x00100000
-           ,FUNC_SMBUS_READ_WORD_DATA   = 0x00200000
-           ,FUNC_SMBUS_WRITE_WORD_DATA  = 0x00400000
-           ,FUNC_SMBUS_PROC_CALL        = 0x00800000
-           ,FUNC_SMBUS_READ_BLOCK_DATA  = 0x01000000
-           ,FUNC_SMBUS_WRITE_BLOCK_DATA = 0x02000000
-           ,FUNC_SMBUS_READ_I2C_BLOCK   = 0x04000000 /* I2C-like block xfer  */
-           ,FUNC_SMBUS_WRITE_I2C_BLOCK  = 0x08000000 /* w/ 1-byte reg. addr. */
+            FuncI2C                  = 0x00000001
+           ,Func10BitAddr            = 0x00000002
+           ,FuncProtocolMangling     = 0x00000004 /* I2C_M_IGNORE_NAK etc. */
+           ,FuncSmbusPec             = 0x00000008
+           ,FuncNostart              = 0x00000010 /* I2C_M_NOSTART */
+           ,FuncSmbusBlockProcCall   = 0x00008000 /* SMBus 2.0 */
+           ,FuncSmbusQuick           = 0x00010000
+           ,FuncSmbusReadByte        = 0x00020000
+           ,FuncSmbusWriteByte       = 0x00040000
+           ,FuncSmbusReadByteData    = 0x00080000
+           ,FuncSmbusWriteByteData   = 0x00100000
+           ,FuncSmbusReadWordData    = 0x00200000
+           ,FuncSmbusWriteWordData   = 0x00400000
+           ,FuncSmbusProcCall        = 0x00800000
+           ,FuncSmbusReadBlockData   = 0x01000000
+           ,FuncSmbusWriteBlockData  = 0x02000000
+           ,FuncSmbusReadI2CBlock    = 0x04000000 /* I2C-like block xfer  */
+           ,FuncSmbusWriteI2CBlock   = 0x08000000 /* w/ 1-byte reg. addr. */
+        };
+
+        enum I2CControlCodes {
+             CodeI2CRetries     = 0x0701
+            ,CodeI2CTimeout     = 0x0702
+            ,CodeI2CSlave       = 0x0703
+            ,CodeI2CSlaveForce  = 0x0706
+            ,CodeI2CTenBit      = 0x0704
+            ,CodeI2CPec         = 0x0708
         };
 
         Q_ENUMS( BusIdInvalidValue )
         Q_ENUMS( SlaveAddrInvalidValue )
         Q_ENUMS( I2CFunctionCodes )
+        Q_ENUMS( I2CControlCodes )
 
         quint32 busId( ) const;
         void setBusId( quint32 value );
