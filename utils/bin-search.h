b51241b98c30ac3786c3be9fe6bb068241e5fd8f
newenclave <pikratacid@gmail.com>
2014-12-24 16:31:42 +0300

    agent; lua

---
+++ utils/bin-search.h
@@ -0,0 +1,64 @@
+#ifndef FR_BIN_SEARCH_H
+#define FR_BIN_SEARCH_H
+
+#include <iterator>
+
+namespace fr { namespace utils {
+
+    template <typename IterType, typename ValueType>
+    const IterType bin_search( IterType begin, IterType end, ValueType value )
+    {
+        IterType e(end);
+        while( begin != end ) {
+            IterType m( begin );
+            std::advance( m, (std::distance( begin, end ) >> 1) );
+            const ValueType &val(*m);
+            if( val < value ) {
+                begin = m;
+                std::advance( begin, 1 );
+            } else if( value < val ) {
+                end = m;
+            } else {
+                return m;
+            }
+        }
+        return e;
+    }
+
+    template <typename IterType, typename ValueType>
+    IterType bin_lower_bound( IterType begin, IterType end, ValueType value )
+    {
+        while( begin != end ) {
+            IterType m( begin );
+            std::advance( m, ( std::distance( begin, end ) >> 1 ) );
+            const ValueType &val = *m;
+            if( val < value ) {
+                begin = m;
+                std::advance( begin, 1 );
+            } else {
+                end = m;
+            }
+        }
+        return begin;
+    }
+
+    template <typename IterType, typename ValueType>
+    IterType bin_upper_bound( IterType begin, IterType end, ValueType value )
+    {
+        while( begin != end ) {
+            IterType m( begin );
+            std::advance( m, ( std::distance( begin, end ) >> 1 ) );
+            const ValueType &val = *m;
+            if( value < val  ) {
+                end = m;
+            } else  {
+                begin = m;
+                std::advance( begin, 1 );
+            }
+        }
+        return begin;
+    }
+
+}}
+
+#endif // BINSEARCH_H
