60864fb9b9f9aac8d4c2b7e5a459536889093c64
newenclave <pikratacid@gmail.com>
2015-03-17 10:04:09 +0300

    lua wrappers

--- console-client/lua-os.cpp
+++ console-client/lua-os.cpp
@@ -41,15 +41,12 @@ namespace fr { namespace lua {
             lua::objects::table_sptr init_table( )
             {
                 objects::table_sptr ost(objects::new_table( ));
-                ost->add(
-                        objects::new_string( system_call_name ),
-                        objects::new_function( &lcall_os_exec ))
-                   ->add(
-                        objects::new_string( exec_call_name ),
-                        objects::new_function( &lcall_os_exec ))
-                   ->add(
-                        objects::new_string( names::inst_field ),
-                        objects::new_light_userdata( iface_.get( ) ) );
+                ost->add( system_call_name,
+                          objects::new_function( &lcall_os_exec ))
+                   ->add( exec_call_name,
+                          objects::new_function( &lcall_os_exec ))
+                   ->add( names::inst_field,
+                          objects::new_light_userdata( iface_.get( ) ) );
 
                 return ost;
             }
@@ -61,7 +58,9 @@ namespace fr { namespace lua {
         const char *table_path( )
         {
             static const std::string path =
-                    std::string( names::client_table ) + '.' + table_name( );
+                         std::string( names::client_table )
+                       + '.'
+                       + table_name( );
             return path.c_str( );
         }
         data_sptr init( std::shared_ptr<lua::state> &ls,
