d674a9b76bef7636bcacefc47990e1717898e77c
newenclave <pikrat.acid@gmail.com>
2015-03-17 15:44:47 +0300

    lua client

---
+++ lua-client/fr-lua.h
@@ -0,0 +1,10 @@
+#ifndef FR_CLIENT_LUA_H
+#define FR_CLIENT_LUA_H
+
+#define LUA_WRAPPER_TOP_NAMESPACE fr
+
+#include "fr-lua/lua-wrapper.hpp"
+#include "fr-lua/lua-type-wrapper.hpp"
+#include "fr-lua/lua-objects.hpp"
+
+#endif // FR_CLIENT_LUA_H
---
+++ lua-client/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "fr-lua.h"
+
+#include "vtrc-common/vtrc-thread-pool.h"
+#include "vtrc-common/vtrc-pool-pair.h"
+
+#include "vtrc-client/vtrc-client.h"
+
+namespace vcomm     = vtrc::common;
+namespace vclient   = vtrc::client;
+
+int main(  )
+{
+
+    return 0;
+}
