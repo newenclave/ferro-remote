17fd9ae7309aabdcde68581debb488f1469be2aa
newenclave <pikrat.acid@gmail.com>
2015-03-17 15:57:56 +0300

    lua client

--- lua-client/main.cpp
+++ lua-client/main.cpp
@@ -7,8 +7,36 @@
 
 #include "vtrc-client/vtrc-client.h"
 
+#include "boost/program_options.hpp"
+
 namespace vcomm     = vtrc::common;
 namespace vclient   = vtrc::client;
+
+namespace po = boost::program_options;
+
+namespace {
+
+    void fill_common_options( po::options_description &desc )
+    {
+        desc.add_options( )
+
+            ("help,h",   "help message")
+
+            ("script,e", po::value<std::string>( ), "path to script" )
+
+            ("server,s", po::value<std::string>( ),
+                    "endpoint name; <tcp address>:<port> or <file name>")
+
+            ("id", po::value<std::string>( ),
+                    "set client ID for the remote agent")
+
+            ("key", po::value<std::string>( ),
+                    "set client KEY for the remote agent")
+
+            ;
+    }
+
+}
 
 int main(  )
 {
