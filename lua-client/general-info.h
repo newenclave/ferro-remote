fc745f6300923fefeed6ddc4a19ffffde8c1d42d
newenclave <pikrat.acid@gmail.com>
2015-03-18 10:24:40 +0300

    lua client

--- lua-client/general-info.h
+++ lua-client/general-info.h
@@ -1,5 +1,12 @@
 #ifndef FR_LUA_CLIENT_GENERALINFO_H
 #define FR_LUA_CLIENT_GENERALINFO_H
 
+namespace fr { namespace client { namespace lua {
+
+    struct general_info {
+
+    };
+
+}}}
 
 #endif // FR_LUA_CLIENT_GENERALINFO_H
--- lua-client/main.cpp
+++ lua-client/main.cpp
@@ -13,8 +13,14 @@ namespace vcomm     = vtrc::common;
 namespace vclient   = vtrc::client;
 
 namespace po = boost::program_options;
+using namespace fr;
 
 namespace {
+
+    void show_help( po::options_description &description )
+    {
+        std::cout << description << "\n";
+    }
 
     void fill_common_options( po::options_description &desc )
     {
@@ -32,7 +38,6 @@ namespace {
 
             ("key", po::value<std::string>( ),
                     "set client KEY for the remote agent")
-
             ;
     }
 
@@ -40,6 +45,30 @@ namespace {
 
 int main( int argc, const char *argv[] )
 {
+    po::options_description description("Allowed common options");
+
+    fill_common_options( description );
+
+    vcomm::pool_pair pp( 1, 1 );
+
+    po::variables_map vm;
+
+    try {
+        po::options_description desc = description;
+        po::parsed_options parsed (
+            po::command_line_parser( argc, argv )
+                .options(desc)
+                .allow_unregistered( )
+                .run( ));
+        po::store(parsed, vm);
+    } catch ( const std::exception &ex ) {
+        std::cerr << "Options error: " << ex.what( )
+                  << "\n";
+        show_help( description );
+        return 1;
+    }
+
+    lua::state general_state;
 
     return 0;
 }
