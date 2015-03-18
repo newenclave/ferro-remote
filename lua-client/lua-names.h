5d49806d23f16f68f8c325174b17058ed6092b5a
newenclave <pikrat.acid@gmail.com>
2015-03-18 11:23:36 +0300

    lua client

---
+++ lua-client/lua-names.h
@@ -0,0 +1,10 @@
+#ifndef FR_LUA_NAMES_H
+#define FR_LUA_NAMES_H
+
+#define FR_CLIENT_LUA_MAIN_TABLE "fr"
+#define FR_CLIENT_LUA_HIDE_TABLE "frhide"
+
+#define FR_CLIENT_LUA_CLIENT_TABLE FR_CLIENT_LUA_MAIN_TABLE ".client"
+
+
+#endif // FR_LUA_NAMES_H
