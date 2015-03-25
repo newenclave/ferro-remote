c1b3ceeb15d7d9044ad247bf9fe522b9387d0703
newenclave <pikrat.acid@gmail.com>
2015-03-25 18:01:02 +0300

    lua client

--- lua-client/test2.lua
+++ lua-client/test2.lua
@@ -4,53 +4,53 @@ eq   = fr.client.event_queue
 counter = { tasks = 0 }
 
 function starttask( )
-	counter.tasks = counter.tasks + 1 
+    counter.tasks = counter.tasks + 1 
 end
 
 function stoptask( )
-	counter.tasks = counter.tasks - 1 
-	if counter.tasks == 0 then 
-		fr.exit( 0 )
-	end
+    counter.tasks = counter.tasks - 1 
+    if counter.tasks == 0 then 
+	fr.exit( 0 )
+    end
 end
 
 function get_file( path )
-	local function impl( info )
-		local d, err = file.read( info.id, 1 )
-		if err then
-			fr.print( "read error: ", err, "\n" )
-			stoptask( )
-			return
-		end 
-		if string.len(d) ~= 0  then
-			fr.print( "got data: ", info["path"], "; '", d, "'\n" )
-			info.total = info.total + string.len(d)
-			eq.post( impl, info )
-		else 
-			stoptask( )
-			fr.print( "get file "..info["path"].." complete. total bytes: ", info.total, "\n" )
-		end
-	end
-	local info = { id = nil, ["path"] = path, total = 0 }
-	info.id, err = file.open( path )
-	if err then 
-	    fr.print( "open error: ", path, "; ", err, "\n" )
+    local function impl( info )
+	local d, err = file.read( info.id, 1 )
+	if err then
+	    fr.print( "read error: ", err, "\n" )
+	    stoptask( )
 	    return
+	end 
+	if string.len(d) ~= 0  then
+	    fr.print( "got data: ", info["path"], "; '", d, "'\n" )
+	    info.total = info.total + string.len(d)
+	    eq.post( impl, info )
+	else 
+	    stoptask( )
+	    fr.print( "get file "..info["path"].." complete. total bytes: ", info.total, "\n" )
 	end
-	starttask( )
-	eq.post( impl, info )
-	
+    end
+    local info = { id = nil, ["path"] = path, total = 0 }
+    info.id, err = file.open( path )
+    if err then 
+        fr.print( "open error: ", path, "; ", err, "\n" )
+        return
+    end
+    starttask( )
+    eq.post( impl, info )
+    
 end
 
 function main ( argv ) 
-	get_file( "/home/data/tst.txt" )
-	get_file( "/home/data/tst2.txt" )
-	local f = file.open( "/dev/random" )
-	if f ~= nil then
-		starttask( )
-		local res, err = file.subscribe( f, "on_pollin", function( data ) fr.print( data, "\n" ) end )
-		eq.timer.post( stoptask, 5 )
-		fr.print( "subscribed: ", res, ": ", err, "\n" )
-	end
+    get_file( "/home/data/tst.txt" )
+    get_file( "/home/data/tst2.txt" )
+    local f = file.open( "/dev/random" )
+    if f ~= nil then
+	starttask( )
+	local res, err = file.subscribe( f, "on_pollin", function( data ) fr.print( data, "\n" ) end )
+	eq.timer.post( stoptask, 5 )
+	fr.print( "subscribed: ", res, ": ", err, "\n" )
+    end
 end
 
