b4acf5079ec0e2b3a2b30fae0189824e2f64ca24
newenclave <pikrat.acid@gmail.com>
2015-04-22 16:46:41 +0300

    base iface for ifaceses

---
+++ client-core/impl/impl-base.cpp
@@ -0,0 +1,7 @@
+
+#include "client-core/interfaces/IBaseIface.h"
+
+namespace fr { namespace client { namespace interfaces {
+
+
+}}}
---
+++ include/client-core/interfaces/IBaseIface.h
@@ -0,0 +1,15 @@
+#ifndef FR_IBASEIFACE_H
+#define FR_IBASEIFACE_H
+
+#include "vtrc-function.h"
+
+namespace fr { namespace client { namespace interfaces {
+
+    struct base {
+        virtual ~base( ) { }
+    };
+}
+
+}}
+
+#endif // FR_IBASEIFACE_H
