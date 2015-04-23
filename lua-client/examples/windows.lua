f94f8c7aa257ec4a54f5c4129ac9ffee7c0386a5
newenclave <pikrat.acid@gmail.com>
2015-04-23 14:58:22 +0300

    lua

--- lua-client/examples/windows.lua
+++ lua-client/examples/windows.lua
@@ -4,11 +4,15 @@ cf = con.flush
 window = { 
     x = 0, y = 0, 
     children = { },    
-    h = con.size( ).height, w = con.size( ).width
+    h = con.size( ).height - 3, w = con.size( ).width
 }
 
 window.size = function ( self )
-    return self.w, self.h
+    if self.parent then
+        return self.w, self.h
+    else 
+	return con.size( ).width, con.size( ).height - 3 
+    end
 end
 
 window.add = function ( self, new_wind )
@@ -86,23 +90,51 @@ window.draw = function( self )
     P( x + w, y     ) W:write( "+" )
     P( x,     y + h ) W:write( "+" )
     P( x + w, y + h ) W:write( "+" )
+    
+    if self.cdraw then 
+	self:cdraw( )
+    end	
+    
     for k, v in pairs( self.children ) do 
 	v:draw( )
     end
     io.stdout:flush( )
 end
 
-function draw_all(  )
+function draw_all( err, win )
     con.clear( )
-    window:draw( )
-    fr.client.event_queue.timer.post(draw_all, {0, 100} )
+    win:draw( )
+    fr.client.event_queue.timer.post(draw_all, {0, 100}, win )
+end
+
+function timer( err, win )
+    win.value = win.value + 1
+    status.text =  "progress: "..tostring(win.value)..":"..tostring(win.w - 1)
+    if win.value > (win.w - 2) then
+	win.value = 0
+    end
+    win:draw( )
+    fr.client.event_queue.timer.post( timer, {0, 50}, win )
 end
 
 function main( )
     con.clear( )
     
     local h = window:new( )
+    status = window:new( )
+    status.text = ""
+    status.size = function( )
+	return con.size( ).width, 3
+    end
+    status.pos = function( )
+	return 0, con.size( ).height - 3 
+    end
+    status.cdraw = function( self )
+	con.set_pos( 2, con.size( ).height - 2 )
+	io.stdout:write( self.text )
+    end   
 
+ 
     h.x, h.y = 5,  5
     h.w, h.h = 10, 5
 
@@ -111,8 +143,31 @@ function main( )
 
     local h3 = h2:clone( )
     h3.x, h3.y = 25, 8 
+    
+    local h4 = h:clone( )
+    h4.x, h4.y = 10,  1
+    h4.w, h4.h = 35, 2 
+    h4.value = 0    
 
-    draw_all( window ) 
+    h4.cdraw = function( self )
+	con.set_pos( self.x + 1, self.y + 1 )
+
+	if self.value < ( self.w // 3 ) then
+	    con.set_color( "green" )
+	elseif self.value < ( self.w - self.w // 3 )  then 
+	    con.set_color( "yellow" )
+	else 
+	    con.set_color( "red" )
+	end
+
+	for i = 0, self.value do 
+	    io.stdout:write( "|" )
+	end
+	con.set_color( )
+    end
+
+    draw_all( nil, window ) 
+    timer( nil, h4 )
 
     fr.run( )
 end
