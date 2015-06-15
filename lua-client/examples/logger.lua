08a623c9c23d2661fa8d8049327ece149dd5e1d9
newenclave <pikrat.acid@gmail.com>
2015-06-15 14:30:06 +0300

    logger RPC

--- lua-client/examples/logger.lua
+++ lua-client/examples/logger.lua
@@ -2,18 +2,6 @@ log = fr.client.logger
 
 function main( )
     fr.run( )
-    fr.print( log, "\n" )
     local lgr, err = log.add( )
-    print( lgr )
-    print(lgr:write( 1, "test" ))
-    print(log.set_level( 4 ))
-    lgr:write( 2, "test" )
-    lgr:write( 3, "test" )
-    lgr:write( "DBG", "test" )
     lgr:subscribe( "on_write", function( data ) fr.print( data.text, "\n" ) end )
-    lgr:write( 2, "sadsf" )
-    local lgr2 = log.add( )
-    lgr2:write( 3, "sadsf2" )
-    fr.print( lgr:events( ), "\n" )
-    fr.print( lgr2:events( ) )
 end
