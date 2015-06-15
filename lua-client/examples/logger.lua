55a43c118898ac430956d4a4c9572f3aa4d41444
newenclave <pikrat.acid@gmail.com>
2015-06-15 17:20:07 +0300

    logger RPC

--- lua-client/examples/logger.lua
+++ lua-client/examples/logger.lua
@@ -4,10 +4,13 @@ timer = fr.client.event_queue.timer
 function main( )
     fr.run( )
     local lgr, err = log.add( )
+    -- timer event. after 1 second process will send log string and exit
     timer.post( function( err, lgr ) 
         lgr:write( "INF", "Client logger: Goodbye!" ) 
         fr.exit( ) 
     end, { sec=1 }, lgr )
+
+    -- logger event handler. 
     lgr:subscribe( "on_write", function( data ) 
         local dt = os.date( "*t", data.time // 1000000 )
         fr.print( dt.hour, ":", dt.min, ":", dt.sec, 
