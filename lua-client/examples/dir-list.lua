95b5564d8b7d65d023b36d9882b9438259d225a2
newenclave <pikrat.acid@gmail.com>
2015-09-08 16:15:02 +0300

    gyro test

--- lua-client/examples/dir-list.lua
+++ lua-client/examples/dir-list.lua
@@ -2,13 +2,13 @@ client = fr.client
 
 function main( argv )
 		if not fr.client.connected then
-				fr.print( "Client is not connected. Use -s parameter for setup connection\n " )
-				fr.exit( -1 )
-				return
+            fr.print( "Client is not connected. Use -s parameter for setup connection\n " )
+            fr.exit( -1 )
+            return
 		end
 		local path = argv[1]
 		if not path then 
-				fr.print( "Path is not specified. Use -p parameter for setup path\n" )
+            fr.print( "Path is not specified. Use -p parameter for setup path\n" )
 		end
 		client.fs.cd( path )
 		fr.print( "Cd: ", path, "\n" )  
@@ -16,8 +16,8 @@ function main( argv )
 		local i = iterators.begin( )
 		fr.print(iterators.get( i ), "\n")
 		while iterators.has_next( i ) do 
-				iterators.next( i )
-				fr.print(iterators.get( i ), "\n")
+            iterators.next( i )
+            fr.print(iterators.get( i ), "\n")
 		end
 		fr.exit(0)
 end
