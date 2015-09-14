62fc0051c5666f11339a760c99838cbab1a45868
newenclave <pikrat.acid@gmail.com>
2015-09-14 15:38:53 +0300

    c++11

--- client-core/impl/impl-i2c.cpp
+++ client-core/impl/impl-i2c.cpp
@@ -10,16 +10,16 @@
 
 namespace fr { namespace client { namespace interfaces { namespace i2c {
 
-    namespace i2cproto = proto::i2c;
-    namespace vcomm    = vtrc::common;
-    typedef   vcomm::rpc_channel* channel_ptr;
-
-    typedef vcomm::rpc_channel                            channel_type;
-    typedef i2cproto::instance::Stub                      stub_type;
-    typedef vcomm::stub_wrapper<stub_type, channel_type>  client_type;
-    static const unsigned nw_flag = vcomm::rpc_channel::DISABLE_WAIT;
-
     namespace {
+
+        namespace i2cproto = proto::i2c;
+        namespace vcomm    = vtrc::common;
+        typedef   vcomm::rpc_channel* channel_ptr;
+
+        typedef vcomm::rpc_channel                            channel_type;
+        typedef i2cproto::instance::Stub                      stub_type;
+        typedef vcomm::stub_wrapper<stub_type, channel_type>  client_type;
+        static const unsigned nw_flag = vcomm::rpc_channel::DISABLE_WAIT;
 
         unsigned open_device( client_type &cl,
                               unsigned bus_id,
