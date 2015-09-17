9b5a603caba028b4ff8fa8da9e4897557ea22afc
newenclave <pikrat.acid@gmail.com>
2015-09-17 15:07:12 +0300

    lua struct

--- lua-client/examples/tty-events.lua
+++ lua-client/examples/tty-events.lua
@@ -2,7 +2,7 @@ fs = fr.client.fs
 file = fs.file
 
 function main( )
-    fr.run( )
+    fr.run( ) 
     local f = assert(file.open( "/dev/pts/0" ))
     assert(f:subscribe( "on_pollin", function( data ) print(data.data) end, f ))
 end
