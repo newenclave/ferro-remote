2be9b990c1c1ef3c61ca9c4b4f894d344916f4b2
newenclave <pikrat.acid@gmail.com>
2015-09-23 15:41:07 +0300

    qml spi

---
+++ gui-client/fr-client-spi.cpp
@@ -0,0 +1,13 @@
+#include <QSharedPointer>
+#include <iostream>
+
+#include "fr-client-spi.h"
+
+#include "client-core/interfaces/ISPI.h"
+#include "fr-qml-call-wrappers.h"
+
+
+namespace fr { namespace declarative {
+
+
+}}
---
+++ gui-client/fr-client-spi.h
@@ -0,0 +1,16 @@
+#ifndef FRCLIENTSPI_H
+#define FRCLIENTSPI_H
+
+
+#include "fr-client-component.h"
+
+namespace fr { namespace declarative {
+
+    class FrClientSPI: public FrClientComponent
+    {
+        Q_OBJECT
+    };
+
+}}
+
+#endif // FRCLIENTSPI_H
