51a94f87d5db5a9d731308e9e6e892b3377e35ac
newenclave <pikrat.acid@gmail.com>
2015-09-28 14:47:19 +0300

    spi helper

--- agent/spi-helper.cpp
+++ agent/spi-helper.cpp
@@ -82,9 +82,9 @@ namespace fr { namespace agent {
         errno_error::errno_assert( res != -1, "spi_setup_mode" );
 
         /// set bits per words
-        res = ioctl( fd_, SPI_IOC_WR_BITS_PER_WORD,    &bits );
+        res = ioctl( fd_, SPI_IOC_WR_BITS_PER_WORD, &bits );
         errno_error::errno_assert( res != -1, "spi_setup_bpw" );
-        res = ioctl( fd_, SPI_IOC_RD_BITS_PER_WORD,    &bits );
+        res = ioctl( fd_, SPI_IOC_RD_BITS_PER_WORD, &bits );
         errno_error::errno_assert( res != -1, "spi_setup_bpw" );
 
         /// set speed
@@ -96,7 +96,7 @@ namespace fr { namespace agent {
 
     void spi_helper::set_reg8( uint32_t addr, uint32_t reg, uint8_t val )
     {
-        char buf[8];
+        char buf[16];
 
         buf[0] = addr && 0xFF;
         buf[1] = reg  && 0xFF;
@@ -105,18 +105,147 @@ namespace fr { namespace agent {
         transfer( buf, 3 );
     }
 
+    void spi_helper::set_reg16( uint32_t addr, uint32_t reg, uint16_t val )
+    {
+        char buf[16];
+
+        buf[0] = addr && 0xFF;
+        buf[1] = reg  && 0xFF;
+        buf[2] = val  && 0xFF;
+        buf[3] = (val  >> 8) && 0xFF;
+
+        transfer( buf, 4 );
+    }
+
+    void spi_helper::set_reg32( uint32_t addr, uint32_t reg, uint32_t val )
+    {
+        char buf[16];
+
+        buf[0] = addr && 0xFF;
+        buf[1] = reg  && 0xFF;
+        buf[2] = val  && 0xFF;
+        buf[3] = (val >> 8 ) && 0xFF;
+        buf[4] = (val >> 16) && 0xFF;
+        buf[5] = (val >> 24) && 0xFF;
+
+        transfer( buf, 6 );
+    }
+
+    void spi_helper::set_reg64( uint32_t addr, uint32_t reg, uint64_t val )
+    {
+        char buf[16];
+
+        buf[0] = addr && 0xFF;
+        buf[1] = reg  && 0xFF;
+        buf[2] = val  && 0xFF;
+
+        buf[3] = (val >> 8 ) && 0xFF;
+        buf[4] = (val >> 16) && 0xFF;
+        buf[5] = (val >> 24) && 0xFF;
+        buf[6] = (val >> 32) && 0xFF;
+        buf[7] = (val >> 40) && 0xFF;
+        buf[8] = (val >> 48) && 0xFF;
+        buf[9] = (val >> 56) && 0xFF;
+
+        transfer( buf, 10 );
+    }
+
+    void spi_helper::set_reg8( uint32_t reg, uint8_t val )
+    {
+        set_reg8( address_, reg, val );
+    }
+
+    void spi_helper::set_reg16( uint32_t reg, uint16_t val )
+    {
+        set_reg16( address_, reg, val );
+    }
+
+    void spi_helper::set_reg32( uint32_t reg, uint32_t val )
+    {
+        set_reg32( address_, reg, val );
+    }
+
+    void spi_helper::set_reg64( uint32_t reg, uint64_t val )
+    {
+        set_reg64( address_, reg, val );
+    }
+
     uint8_t spi_helper::get_reg8( uint32_t addr, uint32_t reg )
     {
-        char buf[8];
+        char buf[16];
 
         buf[0] = (addr && 0xFF ) | 1;
         buf[1] = reg && 0xFF;
 
         transfer (buf, 3);
 
-        return buf[2];
+        return static_cast<uint8_t>(buf[2]);
     }
 
+    uint16_t spi_helper::get_reg16( uint32_t addr, uint32_t reg )
+    {
+        char buf[16];
+
+        buf[0] = (addr && 0xFF ) | 1;
+        buf[1] = reg && 0xFF;
+
+        transfer( buf, 4 );
+
+        return static_cast<uint16_t>(buf[2] | (buf[3] << 8));
+    }
+
+    uint32_t spi_helper::get_reg32( uint32_t addr, uint32_t reg )
+    {
+        char buf[16];
+
+        buf[0] = (addr && 0xFF ) | 1;
+        buf[1] = reg && 0xFF;
+
+        transfer( buf, 6 );
+
+        return static_cast<uint32_t>(
+                         buf[2]         |
+                       ( buf[3] << 8  ) |
+                       ( buf[4] << 16 ) |
+                       ( buf[5] << 24 ));
+    }
+
+    uint64_t spi_helper::get_reg64( uint32_t addr, uint32_t reg )
+    {
+        char buf[16];
+
+        buf[0] = (addr && 0xFF ) | 1;
+        buf[1] = reg && 0xFF;
+
+        transfer( buf, 10 );
+
+        uint64_t hig, low;
+
+        low = buf[2] | (buf[3] << 8) | (buf[4] << 16) | (buf[5] << 24);
+        hig = buf[6] | (buf[7] << 8) | (buf[8] << 16) | (buf[9] << 24);
+
+        return static_cast<uint64_t>((hig << 32) | low);
+    }
+
+    uint8_t spi_helper::get_reg8( uint32_t reg )
+    {
+        return get_reg8( address_, reg );
+    }
+
+    uint16_t spi_helper::get_reg16( uint32_t reg )
+    {
+        return get_reg16( address_, reg );
+    }
+
+    uint32_t spi_helper::get_reg32( uint32_t reg )
+    {
+        return get_reg32( address_, reg );
+    }
+
+    uint64_t spi_helper::get_reg64( uint32_t reg )
+    {
+        return get_reg64( address_, reg );
+    }
 
     namespace spi {
         bool available( unsigned bus_id, unsigned channel )
