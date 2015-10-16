8220750b2cfd8744c587d783e3ce80eaf580e41e
newenclave <pikrat.acid@gmail.com>
2015-10-16 18:12:11 +0300

    qml spi

--- lua-client/examples/spidev.lua
+++ lua-client/examples/spidev.lua
@@ -1,6 +1,21 @@
-spidev = { }
+spidev = { 
+    bus	    = 0
+    channel = 0
+}
 
 spidev.new = function( bus, channel )
+    local spi = fr.client.spi
+    local dev = assert(spi.open( bus, channel ))
     intst = { }
+    inst.bus     = bus
+    inst.channel = channel
+    inst.dev     = dev
+    for k, v in pairs(window) do inst[k] = v end
     
+    return inst
 end
+
+spidev.transfer = function( self, data )
+    return self.dev:transfer( data )
+end
+
