6064c9232bbf2146f981a46403939010d7e676ac
VK <kettunen.villu@gmail.com>
2016-07-31 01:23:25 +0300

    sonic lua

--- lua-client/examples/sonic-hc-SR04.lua
+++ lua-client/examples/sonic-hc-SR04.lua
@@ -5,13 +5,13 @@ local ECHO = 3
 local last_tick = 0 
 
 function handler( data )
-    -- fr.print( data )
     if last_tick ~= 0 then 
 	local val = data.value
 	if val == 1 then 
 	    print( "signal sent!" )
 	else  
-	    print( "echo received; distance = ", (data.interval - last_tick)/58, "cm"  )
+	    print( "echo received; distance = ", 
+                   (data.interval - last_tick) / 58, "cm"  )
 	    fr.exit( )
 	end
     end   
@@ -22,10 +22,10 @@ end
 
 function main( )
     fr.run( )  
-    T = assert(gpio.export(TRIG, "out", 0))
-    E = assert(gpio.export(ECHO, "in",  1))
+    T = assert(gpio.export(TRIG, "out"))
+    E = assert(gpio.export(ECHO, "in"))
     assert(E:set( "edge", "both" ))
-    assert(E:subscribe( "on_changed", handler, "E" ))
+    assert(E:subscribe( "on_changed", handler, E, T ))
     assert(T:pulse( 50 ))
 end
 
